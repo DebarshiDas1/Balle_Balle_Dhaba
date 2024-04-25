85ac93d0c1179c1fb1fd350c3f6d994a6e7f00f9
DebarshiDas1 <94341651+DebarshiDas1@users.noreply.github.com>
2024-04-25 13:42:41 +0530

    Upload files from codezen

---
+++ API/Models/AppSetting.g.cs
@@ -0,0 +1,9 @@
+namespace BalleBalleDhaba.Models
+{
+    public static class AppSetting
+    {
+        public static string? JwtKey { get; set; }
+        public static string? JwtIssuer { get; set; }
+        public static int TokenExpirationtime { get; set; }
+    }
+}
